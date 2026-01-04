afdc9b4a37233802b09ff32b7ca311024e4c83d5
Aulianurfitria15 <aulianurfitria17@gmail.com>
2026-01-04 14:26:49 +0700

    mengubah yop app bar

--- app/src/main/java/com/example/tugasakhirpam/view/component/TopAppBar.kt
+++ app/src/main/java/com/example/tugasakhirpam/view/component/TopAppBar.kt
@@ -8,7 +8,9 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -18,24 +20,38 @@ fun AppTopBar(
     onLogout: (() -> Unit)? = null
 ) {
     TopAppBar(
-        title = { Text(title) },
-        navigationIcon = {
+        title = {
+            Text(
+                text = title,
+                color = Color.White
+            )
+        },
+
+                navigationIcon = {
             if (onBack != null) {
                 IconButton(onClick = onBack) {
                     Icon(
                         imageVector = Icons.Default.ArrowBack,
-                        contentDescription = "Back"
+                        contentDescription = "Back",
+                        tint = Color.White
                     )
                 }
             }
         },
+
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = Color(0xFF4F5F59)
+        ),
+
         actions = {
             if (onLogout != null) {
                 IconButton(onClick = onLogout) {
                     Icon(
                         imageVector = Icons.Default.ExitToApp,
-                        contentDescription = "Logout"
+                        contentDescription = "Logout",
+                        tint = Color.White
                     )
+
                 }
             }
         }
