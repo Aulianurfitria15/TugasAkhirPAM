72511896d84c03c8761fdeda3702597ae6cc42fe
Aulianurfitria15 <aulianurfitria17@gmail.com>
2026-01-04 11:49:11 +0700

    menambahkan user dao

---
+++ app/src/main/java/com/example/tugasakhirpam/data/dao/UserDao.kt
@@ -0,0 +1,16 @@
+package com.example.tugasakhirpam.data.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.example.tugasakhirpam.data.model.User
+
+@Dao
+interface UserDao {
+
+    @Insert
+    suspend fun insertUser(user: User)
+
+    @Query("SELECT * FROM user WHERE username = :username AND password = :password")
+    suspend fun login(username: String, password: String): User?
+}
